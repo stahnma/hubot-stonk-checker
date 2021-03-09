579d13e6847432f7046a6a4f19d8b641ef797bbc
Michael Stahnke <stahnma@devops.af>
2021-03-08 23:51:59 -0600

    Add jsbeautify
    Trying to make my javascript a little prettier.

--- src/stonks.js
+++ src/stonks.js
@@ -26,88 +26,86 @@ if(memeset === undefined)
 else
   memeset = memeset.split(',');
 
-module.exports = function(robot) {
+module.exports = function (robot) {
   if(robot.adapterName === 'slack')
     richtext = true
   else
     richtext = false
-  
+
   if(apiKey === undefined) {
     robot.logger.error("Must set HUBOT_FINNHUB_API_KEY for hubot-stonk-checker to work.");
     return false
   }
-  robot.respond (/sto[c|n]ks? ([-\@\w.]{1,11}?\S$)/i, function(msg) {
+  robot.respond(/sto[c|n]ks? ([-\@\w.]{1,11}?\S$)/i, function (msg) {
     symbol = msg.match[1];
     getStockData(symbol, msg, robot);
   });
-  
-  robot.respond (/memestonks?\S$$/i, function(msg) {
+
+  robot.respond(/memestonks?\S$$/i, function (msg) {
     if(richtext)
       msg.send(":wsb:")
     memeset.forEach(symbol => {
       getStockData(symbol, msg, robot);
-    }); 
-    
+    });
+
   });
 }
 
-
-
 function getStockData(symbol, msg, robot) {
- 
+
   url = 'https://finnhub.io/api/v1/quote'
-    url += '?token=' + apiKey;
-    // If it's a common crypto currency abbreviation, help the user out.
-    if(['doge', 'btc', 'xrp','eth'].includes(symbol)) {
-      symbol +=  '-usd'
-    }
-    symbol = symbol.toUpperCase()
-    url += '&symbol=' + symbol.toUpperCase()
-    robot.logger.debug('Url being called in getStockData is', url)
-    msg.http(url)
-      .get() (function(err,res,body) {
-        // if(parseInt(response["statusCode"]) >= 400) {
-        //   robot.logger.error("Lookup failed. HTTP response code: " + response["statusCode"])
-        //   return false
-        // }
-          
-        result = JSON.parse(body)
-        robot.logger.debug("Body from url:", body);
-        // Body returns
-        // { c: 256.89, h: 296, l: 252.01, o: 282, pc: 193.6, t: 1611878400 }
-        delta = parseFloat(result.c - result.pc).toFixed(3)
-
-        if(delta > 0.0)
-            printdelta = '+' + delta
-        else
-            printdelta = delta
-
-        perc = parseFloat(delta / result.pc * 100).toFixed(3)
-        if(perc > 0.0)
-            printperc = '+' + perc + '%'
-        else
-            printperc = perc + '%'
-
-        message = symbol + ' $' + result.c + '  ($' + printdelta + ' ' + printperc + ')'
-
-        if(richtext) {
-          if(delta > 0.0)
-              message = ":stonks: " + message
-          if (delta < 0.0)
-              message = ":stonks-down: " + message
-          if (delta == 0.0)
-              message = message
-          if (symbol == 'DOGE-USD')
-              message = ":doge: " + message
-          if (perc > 15.00)
-              message = message + "\n :gem: :raised_hands: :rocket: :rocket: :rocket: :moon:"
-        }
-        if(result.pc == 0)
-          message = symbol + " ticker symbol not found."
-
-        msg.send(message)
-    });
+  url += '?token=' + apiKey;
+  // If it's a common crypto currency abbreviation, help the user out.
+  if(['doge', 'btc', 'xrp', 'eth'].includes(symbol)) {
+    symbol += '-usd'
   }
+  symbol = symbol.toUpperCase()
+  url += '&symbol=' + symbol.toUpperCase()
+  robot.logger.debug('Url being called in getStockData is', url)
+  msg.http(url)
+    .get()(function (err, res, body) {
+      // if(parseInt(response["statusCode"]) >= 400) {
+      //   robot.logger.error("Lookup failed. HTTP response code: " + response["statusCode"])
+      //   return false
+      // }
+
+      result = JSON.parse(body)
+      robot.logger.debug("Body from url:", body);
+      // Body returns
+      // { c: 256.89, h: 296, l: 252.01, o: 282, pc: 193.6, t: 1611878400 }
+      delta = parseFloat(result.c - result.pc).toFixed(3)
+
+      if(delta > 0.0)
+        printdelta = '+' + delta
+      else
+        printdelta = delta
+
+      perc = parseFloat(delta / result.pc * 100).toFixed(3)
+      if(perc > 0.0)
+        printperc = '+' + perc + '%'
+      else
+        printperc = perc + '%'
+
+      message = symbol + ' $' + result.c + '  ($' + printdelta + ' ' + printperc + ')'
+
+      if(richtext) {
+        if(delta > 0.0)
+          message = ":stonks: " + message
+        if(delta < 0.0)
+          message = ":stonks-down: " + message
+        if(delta == 0.0)
+          message = message
+        if(symbol == 'DOGE-USD')
+          message = ":doge: " + message
+        if(perc > 15.00)
+          message = message + "\n :gem: :raised_hands: :rocket: :rocket: :rocket: :moon:"
+      }
+      if(result.pc == 0)
+        message = symbol + " ticker symbol not found."
+
+      msg.send(message)
+    });
+}
 
 //   # This can be used for slack formatted cards. I don't actually like it that much.
 //   getSlackData = (symbol, msg) ->
