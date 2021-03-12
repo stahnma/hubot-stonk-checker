07c1b134d46c941f673b2fedf4ef6e9f0ec443ab
Michael Stahnke <stahnma@devops.af>
2021-03-11 22:45:22 -0600

    Cleaning and linting

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-fs = require 'fs'
-path = require 'path'
+'use strict'
 
-module.exports = (robot, scripts) ->
-  scriptsPath = path.resolve(__dirname, 'src')
-  fs.exists scriptsPath, (exists) ->
-    if exists
-      for script in fs.readdirSync(scriptsPath)
-        if scripts? and '*' not in scripts
-          robot.loadFile(scriptsPath, script) if script in scripts
-        else
-          robot.loadFile(scriptsPath, script)
+const path = require('path')
+
+module.exports = (robot) => {
+  const scriptsPath = path.resolve(__dirname, 'src')
+  robot.loadFile(scriptsPath, 'stonks.js')
+}
--- src/stonks.js
+++ src/stonks.js
@@ -6,7 +6,7 @@
 //
 // Configuration:
 //  HUBOT_FINNHUB_API_KEY from finntech.io
-//   HUBOT_MEMESTONKS optional comma seperated list of stocks to check with
+//  HUBOT_MEMESTONKS optional comma seperated list of stocks to check with
 //     memestonk commands
 //
 // Commands:
@@ -15,96 +15,106 @@
 //   hubot memestonks
 //
 
-env = process.env
-var apiKey = process.env.HUBOT_FINNHUB_API_KEY
-var memeset = process.env.HUBOT_MEMESTONKS
-const default_meme_set = "AMC,BB,BBBY,DOGE-USD,GME"
-var richtext
+// const env = process.env
+const apiKey = process.env.HUBOT_FINNHUB_API_KEY;
+let memeset = process.env.HUBOT_MEMESTONKS;
+const defaultMemeSet = 'AMC,BB,BBBY,DOGE-USD,GME';
+let richtext;
 
-if(memeset === undefined)
-  memeset = default_meme_set.split(',');
-else
+if (memeset === undefined) {
+  memeset = defaultMemeSet.split(',');
+} else {
   memeset = memeset.split(',');
+}
 
-module.exports = function (robot) {
-  if(robot.adapterName === 'slack')
-    richtext = true
-  else
-    richtext = false
-
-  if(apiKey === undefined) {
-    robot.logger.error("Must set HUBOT_FINNHUB_API_KEY for hubot-stonk-checker to work.");
-    return false
+module.exports = function(robot) {
+  if (robot.adapterName === 'slack') {
+    richtext = true;
+  } else {
+    richtext = false;
   }
-  robot.respond(/sto[c|n]ks? ([-\@\w.]{1,11}?\S$)/i, function (msg) {
+
+  if (apiKey === undefined) {
+    robot.logger
+        .error('Must set HUBOT_FINNHUB_API_KEY for hubot-stonk-checker to work.');
+    return false;
+  }
+  robot.respond(/sto[c|n]ks? ([-\@\w.]{1,11}?\S$)/i, function(msg) {
     symbol = msg.match[1];
     getStockData(symbol, msg, robot);
   });
 
-  robot.respond(/memestonks?\S$$/i, function (msg) {
-    if(richtext)
-      msg.send(":wsb:")
-    memeset.forEach(symbol => {
+  robot.respond(/memestonks?\S$$/i, function(msg) {
+    if (richtext) {
+      msg.send(':wsb:');
+    }
+    memeset.forEach((symbol) => {
       getStockData(symbol, msg, robot);
     });
-
   });
-}
+};
 
 function getStockData(symbol, msg, robot) {
-
-  url = 'https://finnhub.io/api/v1/quote'
+  url = 'https://finnhub.io/api/v1/quote';
   url += '?token=' + apiKey;
   // If it's a common crypto currency abbreviation, help the user out.
-  if(['doge', 'btc', 'xrp', 'eth'].includes(symbol)) {
-    symbol += '-usd'
+  if (['doge', 'btc', 'xrp', 'eth'].includes(symbol)) {
+    symbol += '-usd';
   }
-  symbol = symbol.toUpperCase()
-  url += '&symbol=' + symbol.toUpperCase()
-  robot.logger.debug('Url being called in getStockData is', url)
+  symbol = symbol.toUpperCase();
+  url += '&symbol=' + symbol.toUpperCase();
+  robot.logger.debug('Url being called in getStockData is', url);
   msg.http(url)
-    .get()(function (err, res, body) {
+      .get()(function(err, res, body) {
       // if(parseInt(response["statusCode"]) >= 400) {
       //   robot.logger.error("Lookup failed. HTTP response code: " + response["statusCode"])
       //   return false
       // }
 
-      result = JSON.parse(body)
-      robot.logger.debug("Body from url:", body);
-      // Body returns
-      // { c: 256.89, h: 296, l: 252.01, o: 282, pc: 193.6, t: 1611878400 }
-      delta = parseFloat(result.c - result.pc).toFixed(3)
+        result = JSON.parse(body);
+        robot.logger.debug('Body from url:', body);
+        // Body returns
+        // { c: 256.89, h: 296, l: 252.01, o: 282, pc: 193.6, t: 1611878400 }
+        delta = parseFloat(result.c - result.pc).toFixed(3);
 
-      if(delta > 0.0)
-        printdelta = '+' + delta
-      else
-        printdelta = delta
+        if (delta > 0.0) {
+          printdelta = '+' + delta;
+        } else {
+          printdelta = delta;
+        }
 
-      perc = parseFloat(delta / result.pc * 100).toFixed(3)
-      if(perc > 0.0)
-        printperc = '+' + perc + '%'
-      else
-        printperc = perc + '%'
+        perc = parseFloat(delta / result.pc * 100).toFixed(3);
+        if (perc > 0.0) {
+          printperc = '+' + perc + '%';
+        } else {
+          printperc = perc + '%';
+        }
 
-      message = symbol + ' $' + result.c + '  ($' + printdelta + ' ' + printperc + ')'
+        message = symbol + ' $' + result.c + '  ($' + printdelta + ' ' + printperc + ')';
 
-      if(richtext) {
-        if(delta > 0.0)
-          message = ":stonks: " + message
-        if(delta < 0.0)
-          message = ":stonks-down: " + message
-        if(delta == 0.0)
-          message = message
-        if(symbol == 'DOGE-USD')
-          message = ":doge: " + message
-        if(perc > 15.00)
-          message = message + "\n :gem: :raised_hands: :rocket: :rocket: :rocket: :moon:"
-      }
-      if(result.pc == 0)
-        message = symbol + " ticker symbol not found."
+        if (richtext) {
+          if (delta > 0.0) {
+            message = ':stonks: ' + message;
+          }
+          if (delta < 0.0) {
+            message = ':stonks-down: ' + message;
+          }
+          if (delta == 0.0) {
+            message = message;
+          }
+          if (symbol == 'DOGE-USD') {
+            message = ':doge: ' + message;
+          }
+          if (perc > 15.00) {
+            message = message + '\n :gem: :raised_hands: :rocket: :rocket: :rocket: :moon:';
+          }
+        }
+        if (result.pc == 0) {
+          message = symbol + ' ticker symbol not found.';
+        }
 
-      msg.send(message)
-    });
+        msg.send(message);
+      });
 }
 
 //   # This can be used for slack formatted cards. I don't actually like it that much.
